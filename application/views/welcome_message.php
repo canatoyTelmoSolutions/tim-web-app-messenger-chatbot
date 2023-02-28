8e0602046ddba85e26fe6ed1bc896321265aefbb
Christian Paranas <paranas.telmosolutions@gmail.com>
2023-02-28 15:56:09 +0800

    init

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -93,6 +93,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="userguide3/">User Guide</a>.</p>
 	</div>
 
+	Eow po
+
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div>
 
