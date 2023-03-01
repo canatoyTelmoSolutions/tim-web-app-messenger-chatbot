fac53e64ea35d44506b1301b860116276a8120e1
Christian Paranas <dev.christianparanas@gmail.com>
2023-03-01 10:57:24 +0800

    add landing page

---
+++ application/views/landing.php
@@ -0,0 +1,31 @@
+<?php $this->load->view('components/header'); ?>
+
+
+
+<div class="">
+	<div class="bg-[#13005A]">
+		<div class="flex justify-between py-4 text-white items-center max-w-[1200px] mx-auto">
+			<div class="font-bold text-2xl">Telmo Chatbot</div>
+
+			<div class="flex items-center font-bold">
+				<div class="cursor-pointer hover:text-gray transition-all">Login</div>
+				<div class="bg-[#212A64] ml-4 py-2 px-4 rounded-md cursor-pointer hover:bg-[#212A80] transition-all">Sign Up</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="">
+		<div class="mt-[100px]">
+			<img 
+				src="<?php echo base_url('assets/images/img_landing_page.png'); ?>"
+			 	alt=""
+				class="w-[500px] mx-auto"
+				>
+		</div>
+	</div>
+
+
+</div>
+
+
+<?php $this->load->view('components/footer'); ?>
