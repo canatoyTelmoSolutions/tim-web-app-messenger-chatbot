594a02e443d680b6a7fc56ca3c8ec08a03523027
Christian Paranas <dev.christianparanas@gmail.com>
2023-03-01 16:45:27 +0800

    current route indicator

--- application/views/components/sidebar.php
+++ application/views/components/sidebar.php
@@ -26,19 +26,19 @@
                         <i class="fal fa-chart-line"></i>
                         <h2 class="text-sm">Dashboard</h2>
 
-                        <div class="absolute hidden right-0 w-1 grow h-[10ppx] bg-[#00FFFF] text-[#00FFFF] rounded-md mr-2 group-hover:block">l</div>
+                        <div class="absolute right-0 w-1 grow h-[10ppx] bg-[#00FFFF] text-[#00FFFF] rounded-md mr-2 group-hover:block <?php if($this->uri->segment(1) == "dashboard") { echo "block"; } else { echo "hidden"; } ?>">l</div>
                     </a>
                     <a href="<?php echo site_url('/user'); ?>" class="group relative grid grid-cols-[30px_1fr] items-center p-2 cursor-pointer hover:bg-[#212A64] rounded-md transition-all hover:p-2 hover:px-3 hover:shadow-md <?php if($this->uri->segment(1) == "user") { echo "bg-[#212A64] px-3 "; }?>">
                         <i class="fal fa-users"></i>
                         <h2 class="text-sm">Users</h2>
 
-                        <div class="absolute hidden right-0 w-1 grow h-[10ppx] bg-[#00FFFF] text-[#00FFFF] rounded-md mr-2 group-hover:block">l</div>
+                        <div class="absolute right-0 w-1 grow h-[10ppx] bg-[#00FFFF] text-[#00FFFF] rounded-md mr-2 group-hover:block <?php if($this->uri->segment(1) == "user") { echo "block"; } else { echo "hidden"; } ?>">l</div>
                     </a>
                     <a href="<?php echo site_url('/chatbot'); ?>" class="group relative grid grid-cols-[30px_1fr] items-center p-2 cursor-pointer hover:bg-[#212A64] rounded-md transition-all hover:p-2 hover:px-3 hover:shadow-md <?php if($this->uri->segment(1) == "chatbot") { echo "bg-[#212A64] px-3 "; }?>">
                         <i class="fal fa-robot"></i>
                         <h2 class="text-sm">Chatbot Settings</h2>
 
-                        <div class="absolute hidden right-0 w-1 grow h-[10ppx] bg-[#00FFFF] text-[#00FFFF] rounded-md mr-2 group-hover:block">l</div>
+                        <div class="absolute right-0 w-1 grow h-[10ppx] bg-[#00FFFF] text-[#00FFFF] rounded-md mr-2 group-hover:block <?php if($this->uri->segment(1) == "chatbot") { echo "block"; } else { echo "hidden"; } ?>">l</div>
                     </a>
                 </div>
             </div>
@@ -51,13 +51,13 @@
                         <i class="fal fa-cog"></i>
                         <h2 class="text-sm">Settings</h2>
 
-                        <div class="absolute hidden right-0 w-1 grow h-[10ppx] bg-[#00FFFF] text-[#00FFFF] rounded-md mr-2 group-hover:block">l</div>
+                        <div class="absolute right-0 w-1 grow h-[10ppx] bg-[#00FFFF] text-[#00FFFF] rounded-md mr-2 group-hover:block <?php if($this->uri->segment(1) == "setting") { echo "block"; } else { echo "hidden"; } ?>">l</div>
                     </a>
                     <div class="group relative grid grid-cols-[30px_1fr] items-center p-2 cursor-pointer hover:bg-[#212A64] rounded-md transition-all hover:p-2 hover:px-3 hover:shadow-md">
                         <i class="fal fa-sign-out"></i>
                         <h2 class="text-sm">Logout</h2>
  
-                        <div class="absolute hidden right-0 w-1 grow h-[10ppx] bg-[#00FFFF] text-[#00FFFF] rounded-md mr-2 group-hover:block">l</div>
+                        <div class="absolute hidden right-0 w-1 grow h-[10ppx] bg-[#00FFFF] text-[#00FFFF] rounded-md mr-2 group-hover:block <?php if($this->uri->segment(1) == "dashboard") { echo "block"; }?>">l</div>
                     </div>
                 </div>
             </div>
