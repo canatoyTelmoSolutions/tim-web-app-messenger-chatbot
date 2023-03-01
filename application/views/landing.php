5536b5b0df52fe4a45c6df00c706629da3b29687
Christian Paranas <dev.christianparanas@gmail.com>
2023-03-01 15:54:20 +0800

    add route link

--- application/views/landing.php
+++ application/views/landing.php
@@ -8,7 +8,7 @@
 			<div class="font-bold text-2xl">Telmo Chatbot</div>
 
 			<div class="flex items-center font-bold">
-				<div class="cursor-pointer hover:text-gray transition-all">Login</div>
+				<a href="<?php echo site_url('/auth'); ?>" class="cursor-pointer hover:text-gray transition-all">Login</a>
 				<a href="<?php echo site_url('/auth'); ?>" class="bg-[#212A64] ml-4 py-2 px-4 rounded-md cursor-pointer hover:bg-[#212A80] transition-all">Sign Up</a>
 			</div>
 		</div>
