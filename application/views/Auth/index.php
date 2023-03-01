5ee761eb22f8d00b01450e1fa06aa92b29198cb6
jech13 <103664029+jech13@users.noreply.github.com>
2023-03-02 07:26:10 +0800

    "login"

--- application/views/Auth/index.php
+++ application/views/Auth/index.php
@@ -1,1 +1,16 @@
-<div class="">Authentication</div>
+<?php $this->load->view('components/header'); ?>
+
+<div class="grid grid-cols-[1fr_900px] h-screen flex ">
+    <div class="bg-[#0E194F] grid place-items-center h-screen">
+        <div class=" pl-10 ">
+            <img src="assets/images/login.svg" alt="" class="content-center pt-10  ">
+        </div>
+    </div>
+    <div class=" grid place-items-center">
+        <div class="px-10">
+            <img src="assets/images/telmo.svg" alt="" />    
+        </div>      
+    </div>
+</div>
+
+<?php $this->load->view('components/footer'); ?>
