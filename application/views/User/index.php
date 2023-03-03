90e2e39193759b7a042d0dc8d6bb23236aff9eb9
Ramel Jay <rameljey15@gmail.com>
2023-03-03 09:03:00 +0800

    pull

--- application/views/User/index.php
+++ application/views/User/index.php
@@ -52,6 +52,10 @@
                                         <? echo $user['gender'] ?>
                                     </td>
                                     <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                                        <span class="inline-block w-1/3 md:hidden font-bold">Home Address</span>
+                                        <? echo $user['address'] ?>
+                                    </td>
+									<td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">
                                         <span class="inline-block w-1/3 md:hidden font-bold">Email Address</span>
                                         <? echo $user['email'] ?>
                                     </td>
