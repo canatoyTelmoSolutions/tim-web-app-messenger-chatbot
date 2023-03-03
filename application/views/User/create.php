eed2e87e6660f3a07a3497ae5e489769b295c846
Ramel Jay <rameljey15@gmail.com>
2023-03-03 10:04:32 +0800

    simple update

--- application/views/User/create.php
+++ application/views/User/create.php
@@ -57,7 +57,7 @@
                     <div class="">
                         <p class="text-sm mb-1">Mobile number</p>
                         <input type="number" name="number" value="<?php echo set_value('number') ?>" placeholder="Mobile number" class="w-full px-3 py-2 bg-gray-200 outline-none rounded-md text-sm 
-                        <? if (form_error('number')) echo 'form_error border border-pink-500'; ?>">
+                        <? if (form_error('number')) echo 'form_error border border-pink-500'; ?>" maxlength="11" oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);">
                         <span class="text-xs text-pink-600"><? echo form_error('number') ?></span>
                     </div>
 
--- application/views/User/index.php
+++ application/views/User/index.php
@@ -45,15 +45,15 @@
                                     </td>
 									<td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">
                                         <span class="inline-block w-1/3 md:hidden font-bold">Age</span>
-                                        <? echo $user['age'] ?>
+                                        <?php echo $user['age'] ?>
                                     </td>
 									<td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">
                                         <span class="inline-block w-1/3 md:hidden font-bold">gender</span>
-                                        <? echo $user['gender'] ?>
+                                        <?php echo $user['gender'] ?>
                                     </td>
                                     <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">
                                         <span class="inline-block w-1/3 md:hidden font-bold">Home Address</span>
-                                        <? echo $user['address'] ?>
+                                        <?php echo $user['address'] ?>
                                     </td>
 									<td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">
                                         <span class="inline-block w-1/3 md:hidden font-bold">Email Address</span>
