afea3e3ef74b3101bf8e012bbfd6e4ae6dd42b25
Ramel Jay <rameljey15@gmail.com>
2023-03-06 08:46:55 +0800

    upstream

--- application/config/database.php
+++ application/config/database.php
@@ -80,7 +80,7 @@ $db['default'] = array(
 	'password' => '',
 	'database' => 'chatbot',
 	'dbdriver' => 'mysqli',
-	'port' => '3306',
+	'port' => '3307',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
