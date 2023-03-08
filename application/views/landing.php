2e937ff40ac5d3502bc40bc4109ac3e6a32ee967
lagunaTelmo <laguna.telmosolutions@gmail.com>
2023-03-08 07:47:06 +0700

    landing page responsive (70%)

--- application/views/landing.php
+++ application/views/landing.php
@@ -1,4 +1,7 @@
 <style>
+    .section{
+        overflow:hidden;
+    }
 	.stage {
         display: flex;
         height: 370px;
@@ -50,36 +53,38 @@
 
 
 
-<div class="bg-gradient-to-br from-white via-sky-100 to-indigo-800 min-h-fit h-screen">
+<div class="bg-gradient-to-br section from-white via-sky-100 to-indigo-800 min-h-fit h-screen">
+<img src="<?php echo base_url('assets/images/o-blobt.png'); ?>"class="w-[350px] mx-auto z-1 absolute lg:h-[150px] md:h-[150px] h-[100px] left-0 top-0  ">
+    <img src="<?php echo base_url('assets/images/o-blobb.png'); ?>"class="w-[600px] mx-auto z-1 absolute right-0 opacity-0 xl:opacity-100 lg:opacity-100 md:opacity-100 lg:h-[390px] md:h-[200px] bottom-0 ">
 	<div class="">
 		<div class="flex justify-between pt-1 px-2 pl-0 text-white items-center max-w-[1200px] mx-auto">
 			<div class="">
-				<img src="<?php echo base_url('assets/images/logo.png'); ?>" alt="" class="w-[60px] md:w-[100px] mx-auto">
+				<img src="<?php echo base_url('assets/images/logo.png'); ?>" alt="" class="w-[60px] relative z-100 md:w-[100px] mx-auto">
 			</div>
 
 			<div class="flex items-center font-bold">
-				<a href="<?php echo base_url('login'); ?>" class="text-sm md:text-md cursor-pointer text-[#212A64] font-bold hover:text-gray transition-all">Login</a>
-				<a href="<?php echo site_url('/auth'); ?>"  class="text-sm md:text-md bg-[#212A64] ml-4 py-2 px-4 rounded-md cursor-pointer hover:bg-[#212A80] transition-all">Login</a>
+				<a href="<?php echo base_url('login'); ?>" class="text-sm md:text-md cursor-pointer text-[#212A64] font-bold hover:text-gray relative z-100 transition-all">Login</a>
+		
 			</div>
 		</div>
 	</div>
 
 	<div class="max-w-[1200px] mx-auto p-2 md:p-0">
-		<div class="mt-[50px] md:grid grid-cols-2 place-items-center">
+		<div class="mt-[50px] md:grid grid-cols-2 place-items-center md:mx-6">
 			<div class="leading-none text-[#0E194F]">
-				<div class="font-bold text-[50px] lg:text-[80px] flex flex-row">Your Business</div>
+				<div class="font-bold text-[50px] lg:text-[75px] flex flex-row">Your Business</div>
 				<!-- <div class="font-bold text-[30px]">Business Our</div> -->
-				<div class="text-[#FD810E] font-bold text-[50px] lg:text-[80px] flex flex-row head"> OUR SOLUTION</div>
+				<div class="text-[#FD810E] font-bold text-[50px] lg:text-[75px] flex flex-row head"> OUR SOLUTION</div>
 
 				<p class="mb-10 font-semibold mt-4">Fostering Innovation in Business</p>
 
-				<a href="<?php echo base_url('login'); ?>" class="text-sm md:text-md font-bold text-white bg-[#0E194F] rounded-2xl py-2 md:py-3 px-4 md:px-6 w-fit hover:bg-slate-800 transition-all cursor-pointer">
+				<a href="<?php echo base_url('login'); ?>" class="text-sm md:text-md font-bold text-white bg-[#0E194F] rounded-2xl py-2 md:py-3 px-4 relative z-100 md:px-6 w-fit hover:bg-slate-800 transition-all cursor-pointer">
 					<span>Get Started</span>
 					<i class="fal fa-arrow-right ml-2 md:ml-4"></i>
 				</a>
 			</div>
-			<div class="stage">
-				<img src="<?php echo base_url('assets/images/landing_illustration.png'); ?>" alt="" class="w-[500px] mx-auto box bounce-1">
+			<div class="stage h-[280px] lg:h-auto">
+				<img src="<?php echo base_url('assets/images/landing_illustration.png'); ?>" alt="" class="w-[390px] lg:w-[500px] relative z-100 mx-auto box bounce-1">
 			</div>
 		</div>
 	</div>
